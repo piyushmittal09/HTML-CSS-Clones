e9ff1b2033ed1f57f661349162b49663975414b0
piyushmittal09 <piyushmittal1983@gmail.com>
2024-10-09 08:23:58 +0530

    complete carousel

---
+++ Amazon/Amazon.js
@@ -0,0 +1,56 @@
+let hamOpen = document.querySelector("#hamburger-open");
+let hamClose = document.querySelector("#hamburger-close");
+let sidebar = document.querySelector(".sidebar");
+const left = document.querySelector(".prev");
+const right = document.querySelector(".next");
+const slider = document.querySelector(".slides");
+let sildeNumber = 1;
+let images = document.querySelectorAll(".slide-images");
+const slideLength = images.length;
+
+
+hamOpen.addEventListener("click", function(){
+  sidebar.classList.toggle("active");
+})
+
+const nextSlide = ()=>{
+  slider.style.transform = `translateX(-${sildeNumber*100}%)`;
+  sildeNumber++;
+  slider.style.transition = "all 1s ease";
+};
+
+const prevSlide = ()=>{
+  slider.style.transform = `translateX(${(sildeNumber-2)*-100}%)`;
+  sildeNumber--;
+  slider.style.transition = "all 1s ease";
+};
+
+const getFirstSlide = ()=>{
+  slider.style.transform = `translateX(0%)`;
+  sildeNumber = 1;
+}
+
+const getLastSlide = ()=>{
+  slider.style.transform = `translateX(-${(length-1)*0}%)`;
+  sildeNumber = length;
+}
+
+right.addEventListener("click", ()=>{
+
+  if(sildeNumber < slideLength){
+    nextSlide();
+  } else{
+    getFirstSlide();
+  }
+  
+});
+
+left.addEventListener("click", ()=>{
+
+  if(sildeNumber > 1){
+    prevSlide();
+  } else{
+    getLastSlide();
+  }
+  
+});
